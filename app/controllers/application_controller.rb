c6b5e43fd18dfda4f905d5d4b8cfb52ae5fcef72
bpereira74 <bpereira74@hotmail.com>
2023-07-31 21:51:08 -0400

    feat: Scaffold comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+      def configure_sign_up_params
+            devise parameter_sanitizer.permit(:sign_up, keys:[:name, :photo])
+      end
+      def configure_update_params
+            devise parameter_sanitizer.permit(:account_update, keys:[:name, :photo])
+      end
 end
